69d3ac1096330e66cbf66b52b58d46a254fbf726
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-30 18:54:38 -0300

    Setting up test routes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const connectDB = require("./connection/db");
 
+const authRoute = require("./routes/auth.routes");
+const userRoute = require("./routes/users.routes");
+const profileRoute = require("./routes/profile.routes");
+const postsRoute = require("./routes/posts.routes");
+
 const app = express();
 connectDB();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/posts", postsRoute);
 
 const PORT = (process.env.PORT = 3333);
 
