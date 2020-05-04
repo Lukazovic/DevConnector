cbb1f1c88cf0c57dc6e4fc3d95493efb99c61778
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-04 12:18:13 -0300

    Add Login form

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -14,12 +15,12 @@ const Register = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       alert("Passwords do not match");
     } else {
-      alert("ok");
+      console.log("Sucess");
     }
   };
 
@@ -76,7 +77,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
