fa848dbf5c2db96d707b35e84631c89a514d3b66
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-07 19:32:04 -0300

    Add ProfileExperience & ProfileEducation to Profile page

--- frontend/src/components/profile/ProfileAbout.js
+++ frontend/src/components/profile/ProfileAbout.js
@@ -7,26 +7,35 @@ const ProfileAbout = ({
     skills,
     owner: { name },
   },
-}) => (
-  <div className="profile-about bg-light p-2">
-    {bio && (
-      <Fragment>
-        <h2 className="text-primary">{name.trim().split(" ")[0]} Bio</h2>
-        <p>{bio}</p>
-        <div className="line" />
-      </Fragment>
-    )}
+}) => {
+  const getFormatedFisrtName = () => {
+    const firstName = `${name.trim().split(" ")[0]}`;
+    if (firstName.endsWith("s")) return `${firstName}'`;
+    return `${firstName}'s`;
+  };
+  const formatedName = getFormatedFisrtName();
 
-    <h2 className="text-primary">Skill Set</h2>
-    <div className="skills">
-      {skills.slice(0, 4).map((skill, index) => (
-        <div key={index} className="p-1">
-          <i className="fa fa-check" /> {skill}
-        </div>
-      ))}
+  return (
+    <div className="profile-about bg-light p-2">
+      {bio && (
+        <Fragment>
+          <h2 className="text-primary">{formatedName} Bio</h2>
+          <p>{bio}</p>
+          <div className="line" />
+        </Fragment>
+      )}
+
+      <h2 className="text-primary">Skill Set</h2>
+      <div className="skills">
+        {skills.slice(0, 4).map((skill, index) => (
+          <div key={index} className="p-1">
+            <i className="fa fa-check" /> {skill}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ProfileAbout.propTypes = {
   profile: PropTypes.object.isRequired,
