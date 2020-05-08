dddcf2a80773fc2ea0b67f2aa11a53402e412816
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-08 11:24:57 -0300

    Add method to add Posts

--- frontend/src/actions/post.js
+++ frontend/src/actions/post.js
@@ -1,7 +1,13 @@
 import api from "../services/api";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+} from "./types";
 
 export const getPosts = () => async dispatch => {
   try {
@@ -46,6 +52,31 @@ export const removeLike = id => async dispatch => {
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    setAuthToken(localStorage.token);
+    const res = await api.post(`/api/posts/`, formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+
+    dispatch(setAlert("Post Created", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
