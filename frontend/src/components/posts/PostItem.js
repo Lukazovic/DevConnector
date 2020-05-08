7db30b0008635d8dae7993a58d65a036ed329b2e
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-08 11:09:43 -0300

    Add Delete Post method

--- frontend/src/components/posts/PostItem.js
+++ frontend/src/components/posts/PostItem.js
@@ -3,50 +3,64 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
-import { addLike, removeLike } from "../../actions/post";
+import { addLike, removeLike, deletePost } from "../../actions/post";
 
 const PostItem = ({
   addLike,
   removeLike,
+  deletePost,
   auth,
   post: { _id, text, name, avatar, owner, likes, comments, date },
 }) => {
   const handleAddLike = e => {
     addLike(_id);
   };
+
   const handleRemoveLike = e => {
     removeLike(_id);
   };
 
+  const handleDeletePost = e => {
+    deletePost(_id);
+  };
+
   return (
-    <div class="post bg-white p-1 my-1">
+    <div className="post bg-white p-1 my-1">
       <div>
         <a href="profile.html">
-          <img class="round-img" src={avatar} alt="" />
+          <img className="round-img" src={avatar} alt="" />
           <h4>{name}</h4>
         </a>
       </div>
       <div>
-        <p class="my-1">{text}</p>
-        <p class="post-date">
+        <p className="my-1">{text}</p>
+        <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
-        <button onClick={handleAddLike} type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-up" />{" "}
+        <button onClick={handleAddLike} type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-up" />{" "}
           {likes.length > 0 && <span>{likes.length}</span>}
         </button>
-        <button onClick={handleRemoveLike} type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-down" />
+        <button
+          onClick={handleRemoveLike}
+          type="button"
+          className="btn btn-light"
+        >
+          <i className="fas fa-thumbs-down" />
         </button>
-        <Link to={`/post/${_id}`} class="btn btn-primary">
+        <Link to={`/post/${_id}`} className="btn btn-primary">
           Discussion{" "}
           {comments.length > 0 && (
-            <span class="comment-count">{comments.length}</span>
+            <span className="comment-count">{comments.length}</span>
           )}
         </Link>
         {!auth.loading && owner === auth.user._id && (
-          <button type="button" class="btn btn-danger">
-            <i class="fas fa-times"></i>
+          <button
+            onClick={handleDeletePost}
+            type="button"
+            className="btn btn-danger"
+          >
+            <i className="fas fa-times"></i>
           </button>
         )}
       </div>
@@ -59,10 +73,13 @@ PostItem.propTypes = {
   auth: PropTypes.object.isRequired,
   addLike: PropTypes.func.isRequired,
   removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+  PostItem
+);
